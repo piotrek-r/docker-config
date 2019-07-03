abb073074ede6dc2a6187555ab1cee2585478508
Piotr Rybałtowski <piotrek@owl-labs.com>
2019-07-03 14:55:02 +0200

    Configure run-server scripts with `set -ex` in 7.2, 7.3

--- configs/php-composer-7.2/run-server.sh
+++ configs/php-composer-7.2/run-server.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 
-/usr/bin/php7.2 -S 0:8080 -t public $@ index.php
+set -ex
+
+/usr/bin/php -S 0:8080 -t public $@ index.php
--- configs/php-composer-7.3/run-server.sh
+++ configs/php-composer-7.3/run-server.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 
+set -ex
+
 /usr/bin/php -S 0:8080 -t public $@ index.php
